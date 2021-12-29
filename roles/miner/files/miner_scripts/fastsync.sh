f0f59efa1e34b5b9c71a20eba97e27a019ca934c
helium <helium@sob.farm>
2021-12-29 00:04:06 -0800

    use .scratch extension while downloading

--- roles/miner/files/miner_scripts/fastsync.sh
+++ roles/miner/files/miner_scripts/fastsync.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 ./height-helper.sh
 snap_height=$(curl -s https://helium-snapshots.nebra.com/latest.json | jq .height)
-docker exec miner wget https://helium-snapshots.nebra.com/snap-${snap_height} -O /var/data/snap/snap-${snap_height}
+docker exec miner wget https://helium-snapshots.nebra.com/snap-${snap_height} -O /var/data/snap/snap-${snap_height}.scratch
+docker exec miner mv /var/data/snap/snap-${snap_height}.scratch /var/data/snap/snap-${snap_height}
 docker exec miner miner repair sync_pause
 docker exec miner miner repair sync_cancel
 docker exec miner miner snapshot load /var/data/snap/snap-${snap_height}
