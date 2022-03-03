495b33f102a13573bcb6ee24f7cb4d65181e39c9
joecryptotoo <80373433+joecryptotoo@users.noreply.github.com>
2022-03-03 13:07:53 -0800

    updated Nebra fastsync endpoint

--- roles/miner/files/miner_scripts/fastsync.sh
+++ roles/miner/files/miner_scripts/fastsync.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 MINER_HEIGHT=$(curl -d '{"jsonrpc":"2.0","id":"id","method":"block_height","params":[]}' -s -o - http://localhost:4467/ | jq .result.height)
-SNAP_HEIGHT=$(curl -s https://helium-snapshots.nebra.com/latest.json | jq .height)
+SNAP_HEIGHT=$(curl -s https://helium-snapshots.nebracdn.com/latest.json | jq .height)
 if [ "${MINER_HEIGHT}" -lt "${SNAP_HEIGHT}" ] ; then
         echo "Downloading snapshot ${SNAP_HEIGHT}"
-        docker exec miner wget https://helium-snapshots.nebra.com/snap-${SNAP_HEIGHT} -O /var/data/snap/snap-${SNAP_HEIGHT}.scratch
+        docker exec miner wget https://helium-snapshots.nebracdn.com/snap-${SNAP_HEIGHT} -O /var/data/snap/snap-${SNAP_HEIGHT}.scratch
         docker exec miner miner repair sync_pause
         docker exec miner miner repair sync_cancel
         docker exec miner mv /var/data/snap/snap-${SNAP_HEIGHT}.scratch /var/data/snap/snap-${SNAP_HEIGHT}
